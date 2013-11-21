940bb9bded8cf7a80141aaccd44d3c91b51b6530
Alvaro Mouriño <alvaro@mourino.net>
2013-11-21 13:09:31 +0100

    Improved pagination for mobiles

--- blatt/www/pagination.py
+++ blatt/www/pagination.py
@@ -26,7 +26,7 @@ class Pagination(object):
         return self.page < self.pages
 
     def iter_pages(self, left_edge=2, left_current=2,
-                   right_current=5, right_edge=2):
+                   right_current=3, right_edge=2):
         last = 0
         for num in xrange(1, self.pages + 1):
             if num <= left_edge or \
