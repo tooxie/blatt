95aeb41263ae120e43583aa5c45c9a8f3b8a2e7a
Alvaro Mouriño <alvaro@mourino.net>
2014-04-11 12:33:09 +0200

    Included meta info in __init__.py file

---
+++ blatt/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Blatt
+#
+# Copyright © 2014 Alvaro Mouriño
+# Author: Alvaro Mouriño <alvaro@mourino.net>
+# URL: <https://github.com/tooxie/blatt>
+"""
+Blatt
+
+Copyright © 2014 Alvaro Mouriño
+"""
+
+__version__ = '0.0.1'
+__author__ = u'Alvaro Mouriño'
+__author_email__ = 'alvaro@mourino.net'
+__copyright__ = '2014 Alvaro Mouriño'
+__url__ = 'https://github.com/tooxie/blatt'
